a37889d8a7e24e888a74644de75a9898ed346f65
Radiyah <rsabur58@gmail.com>
2021-02-28 18:46:48 -0500

    commit tree c3897db4f9166a342a23e414a9c0356e014e2d9c

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,29 @@
+require 'pry'
 
+class Artist
+
+    attr_accessor :name, :artist, :genre
+
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    def initialize(name)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@all << self
+    end
+    # binding.pry
+    def songs
+        self.song
+    end
+
+    def new_song
+    end
+
+    def genres
+    end
+end
